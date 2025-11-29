16a91ec57df60af289276c3e3966c2a74c3a8ac7
Syed Mohammad Askari Abidi <askari.abidi.2005@gmail.com>
2025-11-29 12:23:11 +0530

    added more constants and units

---
+++ sims/basic_physics/two_body_orbit.py
@@ -0,0 +1,7 @@
+import sys, os
+
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../.."))
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
+
+from astro_sims.constants.phys_constants import g
